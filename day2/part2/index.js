f330f3aa44a12a139b70a5b88f8a07f7b492be11
gary-mcmonagle <gary.mcmonagle@gmail.com>
2018-12-02 12:25:15 +0000

    fix daily ordering

--- day2/part2/index.js
+++ day2/part2/index.js
@@ -1,31 +1,35 @@
 const fs = require('fs');
+
 const solve =  async () => {
     const input = await fs.readFileSync("day2/input.txt").toString().split('\n');
-    return input.filter(ins => {
-        return hasRepeating(ins, 2);
-    }).length * 
-    input.filter(ins => {
-        return hasRepeating(ins, 3);
-    }).length
-}
-
-const hasRepeating = (inputString, timesRepeated) => {
-    const charArr = inputString.split('');
-    for(let i = 0; i<charArr.length; i++){
-        let appeared = 0; 
-        for(let j = 0; j<charArr.length; j++){
-            if(charArr[i] === charArr[j]){
-                appeared++; 
+    for(let i = 0; i<input.length; i++){
+        for(let j = 0; j<input.length; j++){
+            let dif =  await differBySingleChar(input[i], input[j])
+            if(dif){
+                return input[i].slice(0,dif-1) + input[i].slice(dif);
             }
         }
-        if(appeared == timesRepeated){
-            return true;
-        }
     }
-    return false;
-
 }
 
+const differBySingleChar = async (truth, compare) => {
+    let diffrence = 0; 
+    let ind = 0
+    for(let i = 0; i<truth.length; i++){
+        if(truth[i] != compare[i]){
+            diffrence++; 
+            ind = i
+        }
+        if(diffrence > 1){
+            return false;
+        }
+    }
+    if(diffrence == 0){
+        return false
+    }
+    return ind
+
+}
 solve().then((output) => {
     console.log(output);
 })
