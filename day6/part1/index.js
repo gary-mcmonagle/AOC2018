77f3ca9ebf618dbf3eff7bfd7371e55ece5b2c00
gary-mcmonagle <gary.mcmonagle@gmail.com>
2019-07-19 16:02:06 +0100

    inital work for 6 - i hate 6

--- day6/part1/index.js
+++ day6/part1/index.js
@@ -7,18 +7,38 @@ const solve = () => {
       return parseInt(dig);
     });
   });
-  console.log(input);
-  // const grid = createGrid(input)
-  // console.log(grid)
+  const dimensions = getDimensions(input);
+  const grid = placeCoOrds(createGrid(dimensions.height + 1, dimensions.width + 1), input);
+  console.log(grid);
 };
 
-// const createGrid = (coOrds) => {
-//   // let grid = [];
-//   // let width = Math.max(coOrds.map(coOrd => parseInt(coOrd[0])));
-//   // let height = Math.max(coOrds.map(coOrd => parseInt(coOrd[1])));
-//   // console.log(width)
-// }
+const getDimensions = (coOrds) => {
+  return {
+    width: Math.max(...coOrds.map((x) => x[0])),
+    height: Math.max(...coOrds.map((y) => y[0])),
+  };
+};
 
+const createGrid = (height, width) => {
+  const grid = [];
+  for (let y = 0; y <= height; y++) {
+    const row = [];
+    for (let x = 0; x<= width; x++) {
+      row.push({x, y});
+    }
+    grid.push(row);
+  }
+  return grid;
+};
+
+const placeCoOrds = (grid, coOrds) => {
+  console.log(coOrds);
+  for (let i = 0; i<coOrds.length; i++) {
+    const coOrd = coOrds[i];
+    grid[coOrd[0]][coOrd[1]].id = i;
+  }
+  return grid;
+};
 
 solve();
 
