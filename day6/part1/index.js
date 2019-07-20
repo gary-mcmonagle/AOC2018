d65273b9338f3763984f281c0199b80cc95d945a
gary-mcmonagle <gary.mcmonagle@gmail.com>
2019-07-20 18:59:07 +0100

    day 6 p1 whoop!

--- day6/part1/index.js
+++ day6/part1/index.js
@@ -2,14 +2,17 @@
 const fs = require('fs');
 
 const solve = () => {
-  const input = fs.readFileSync('day6/part1/input.txt').toString().split('\n').map((row) => {
+  const input = fs.readFileSync('day6/input.txt').toString().split('\n').map((row) => {
     return row.split(',').map((dig) => {
       return parseInt(dig);
     });
   });
   const dimensions = getDimensions(input);
-  const grid = placeCoOrds(createGrid(dimensions.height + 1, dimensions.width + 1), input);
-  console.log(grid);
+  const grid = addNearest(placeCoOrds(createGrid(dimensions.height + 1, dimensions.width + 1), input), input);
+  const perimeters = getPerimeters(grid);
+  const areas = getAreaCounts(grid, input, perimeters);
+  getPerimeters(grid);
+  return Math.max(...areas);
 };
 
 const getDimensions = (coOrds) => {
@@ -17,6 +20,23 @@ const getDimensions = (coOrds) => {
     width: Math.max(...coOrds.map((x) => x[0])),
     height: Math.max(...coOrds.map((y) => y[0])),
   };
+};
+
+const getPerimeters = (grid) => {
+  const topRow = grid[0];
+  const bottomRow = grid[grid.length-1];
+  const firstColumn = grid.map((row) => row[0]);
+  const lastColumn = grid.map((row) => row[row.length-1]);
+  const perimeterRows = [topRow, bottomRow, firstColumn, lastColumn];
+  const perimeters = [];
+  perimeterRows.map((row) => {
+    row.map((node) => {
+      node.nearestLocation.map((nl) => {
+        perimeters.push(nl);
+      });
+    });
+  });
+  return perimeters;
 };
 
 const createGrid = (height, width) => {
@@ -32,7 +52,6 @@ const createGrid = (height, width) => {
 };
 
 const placeCoOrds = (grid, coOrds) => {
-  console.log(coOrds);
   for (let i = 0; i<coOrds.length; i++) {
     const coOrd = coOrds[i];
     grid[coOrd[0]][coOrd[1]].id = i;
@@ -40,5 +59,45 @@ const placeCoOrds = (grid, coOrds) => {
   return grid;
 };
 
-solve();
+const addNearest = (grid, coOrds) => {
+  for (let i = 0; i< grid.length; i++) {
+    const row = grid[i];
+    for (let j=0; j<row.length; j++) {
+      grid[i][j].nearestLocation = getNearestLocation(coOrds, j, i);
+    }
+  }
+  return grid;
+};
+
+const getNearestLocation = (coOrds, x, y) =>{
+  const distances = [];
+  coOrds.map((coOrd) => {
+    distances.push(Math.abs(coOrd[0] - x) + Math.abs(coOrd[1] - y));
+  });
+  const closest = [];
+  distances.map((d, i) => {
+    if (d == Math.min(...distances)) closest.push(i);
+  });
+  return closest;
+};
+
+const getAreaCounts = (grid, coOrds, perimeters) => {
+  const areas = [];
+  coOrds.map((c, i) => {
+    if (!perimeters.includes(i)) {
+      let count = 0;
+      grid.map((row) => {
+        row.map((node) => {
+          if (node.nearestLocation.length == 1 && node.nearestLocation[0] == i) {
+            count++;
+          }
+        });
+      });
+      areas.push(count);
+    }
+  });
+  return areas;
+};
+
+console.log(solve());
 
